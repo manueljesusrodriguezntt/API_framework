b640d2eb8e5506f38285f28289b21528c149ab74
rafaelalcantarantt <ralcantl@emeal.nttdata.com>
2024-09-27 09:40:38 +0200

    added connector to MongoDB

---
+++ src/main/java/com/api/data/connectorMongo/ConnectorMongo.java
@@ -0,0 +1,29 @@
+package com.api.data.connectorMongo;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class ConnectorMongo {
+    public static MongoCollection<Document> connector() {
+        String connectionString = "mongodb+srv://nttprobandoapi:Cartuja2024@cluster0.b9wtp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("variables_entorno");
+        return  database.getCollection("variables");
+    }
+
+}
